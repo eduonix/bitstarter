610adc4cfa3515a884d2d32e646195be8fedf7c9
Eduonix <onixedu@gmail.com>
2013-07-12 18:12:21 +0000

    Trying again

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 var buf = new Buffer('test');
-buf = fs.readFileSync(index.html);
+buf = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
   response.send(buf.toString("utf-8",0,buf.length));
 });
