97eade4e59d84ca80f664a8482465f3f32b9f7ff
Eduonix <onixedu@gmail.com>
2013-07-12 17:44:37 +0000

    Reading index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var buf = new Buffer('test');
+buf = fs.readFileSync(index.html);
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString("utf-8",0,buffer.length);
 });
 
 var port = process.env.PORT || 5000;
