144239352dd13494ad14362cf24fde2704bb06a9
Eduonix <onixedu@gmail.com>
2013-07-13 18:18:37 +0000

    Adding Grader with update

--- grader.js
+++ grader.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 var fs = require('fs');
+var sys = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT= "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+var rest = require('restler');
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)){
@@ -13,6 +14,18 @@ var assertFileExists = function(infile) {
 	}
     return instr;
 };
+var urlData = function(url){
+var result = "Test";
+rest.get('url').on('complete',function(result){
+    if (result instanceof Error) {
+	sys.puts('Error:' + result.message);
+	}else{
+	    sys.puts(result);
+	    }
+});
+return result.toString();
+};
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -41,9 +54,14 @@ if(require.main == module){
     program
     .option('-c, --checks <check_file>','Path to checks.json',clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>' ,'Path to index.html',clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url_link>','Path to html',clone(urlData),HTMLFILE_DEFAULT) 
     .parse(process.argv);
+    if(program.file.length > 2){
     var checkJson = checkHtmlFile(program.file,program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
+    }else{
+	var checkJson = checkHtmlFile(program.url,program.checks);
+}
+var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
