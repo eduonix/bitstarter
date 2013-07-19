a6c0d817eeafcbfa0059a7d759e00b8a0a7b25a3
Ubuntu <ubuntu@ip-10-236-138-162.ec2.internal>
2013-07-19 17:57:43 +0000

    New web.js

--- web.js
+++ web.js
@@ -1,13 +1,17 @@
+#!/usr/bin/env node
 var express = require('express');
 var fs = require('fs');
-var app = express.createServer(express.logger());
-var buf = new Buffer('test');
-buf = fs.readFileSync('index.html');
-app.get('/', function(request, response) {
-  response.send(buf.toString("utf-8",0,buf.length));
-});
+var htmlfile = "index.html";
 
-var port = process.env.PORT || 5000;
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
+  });
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
